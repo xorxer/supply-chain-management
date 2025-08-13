c0e60fff60a406076f902037a00edbfcd9e96d5c
Anthony Le <le672869@gmail.com>
2025-08-13 13:04:27 -0700

    Separated seed data and seed creation

--- backend/prisma/seed.ts
+++ backend/prisma/seed.ts
@@ -1,4 +1,5 @@
 import { PrismaClient } from '@prisma/client';
+import seedData from './seed/data.json';
 
 const prisma = new PrismaClient();
 
@@ -11,37 +12,19 @@ async function main() {
     await prisma.product.deleteMany()
     await prisma.warehouse.deleteMany()
 
-    const productData = [
-        { name: "Laptop", description: "10th generation laptop", category: "Electronics" },
-        { name: "Chair", description: "Comfortable, stury metal chair", category: "Furniture" },
-        { name: "Desk", description: "Office desk", category: "Furniture" },
-        { name: "Headphones", description: "Noise cancelling headphones", category: "Electronics" },
-        { name: "Smartphone", description: "Multi-lens smartphone", category: "Electronics" },
-        { name: "Monitor", description: "4K Ultra HD monitor", category: "Electronics" },
-        { name: "Backpack", description: "Waterproof backpack", category: "Accessories" },
-    ];
+    // Create products
+    for (const productData of seedData.products) {
+        await prisma.product.create({
+            data: productData,
+        });
+    }
 
-    console.log("Seeding products....");
-
-    const products = await Promise.all(
-        productData.map(product => prisma.product.create({ data: product }))
-    )
-  
-    console.log('Seeding warehouses...')
-  
-    const warehouseData = [
-        { name: 'West Coast Distribution Center', location: 'Los Angeles, CA' },
-        { name: 'East Coast Center', location: 'Atlanta, GA' },
-        { name: 'Central Hub', location: 'Chicago, IL' },
-        { name: 'Northwest Hub', location: 'Seattle, WA' },
-        { name: 'Texas Distribution Center', location: 'Dallas, TX' }
-    ]
-
-    const warehouses = await Promise.all(
-        warehouseData.map(warehouse => prisma.warehouse.create({ data: warehouse }))
-    )
-  
-    
+    // Create warehouses
+    for (const warehouseData of seedData.warehouses) {
+        await prisma.warehouse.create({
+            data: warehouseData,
+        })
+    }
 
 
 }
